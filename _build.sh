b6e0e392483e9a03015a324dba45db281896a2d0
Yufree <yufree@live.cn>
2016-09-07 15:45:20 -0400

    add revise

--- _build.sh
+++ _build.sh
@@ -4,13 +4,3 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
 git config --global user.email "yufree@live.cn"
 git config --global user.name "Yufree"
-
-# clone the repository to the book-output directory
-git clone -b gh-pages \
-  https://github.com/yufree/metaworkflow.git \
-  book-output
-cd book-output
-cp -r ../_book/* ./
-git add --all *
-git commit -m"Update the book"
-git push origin gh-pages
